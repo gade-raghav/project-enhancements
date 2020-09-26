498abf0f63eb9cb0710ef879da5cfc0c65c8bb8d
gade-raghav <gaderaghav5@gmail.com>
2020-09-26 11:12:13 +0530

    debug=false

--- enhancements/settings.py
+++ enhancements/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'xyg0obvs+^bn&rrdi3^yc&la%@)lr0@gl$1lmye*e)h9(h9m+u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','enhancements.herokuapp.com']
 
@@ -144,7 +144,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-#MEDIA_URL = '/static/images/'
+MEDIA_URL = '/static/images/'
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
